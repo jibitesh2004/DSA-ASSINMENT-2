28fc908e49a125077d7ecc0619a8e339fafa36fd
JIBITESH KUMAR MISHRA <131876188+jibitesh2004@users.noreply.github.com>
2024-04-18 12:07:57 +0530

    Update H1.java

--- H1.java
+++ H1.java
@@ -1,42 +1,42 @@
 import java.util.*;
-class Commission{
+
+class Commission {
     int sales;
     double Commision;
-    Commission(int sales){
-        if(sales  < 100){
-            this.Commision = 0.2 * sales;
 
+    Commission(int sales) {
+        if (sales < 100) {
+            Commision = 0.02 * sales;
+        } else if (sales == 500 && sales < 5000) {
+            Commision = 0.05 * sales;
+        } else {
+            Commision = 0.08 * sales;
         }
-        else if(sales >=500 && sales <5000){
-            this.Commision = 0.5 * sales;
-        }
-        else{
-            this.Commision = 0.8 * sales;
-        }
-
-
-
-
-  
     }
-    void getCommission(){
+
+    void getCommission() {
         System.out.println(Commision);
-
     }
-
 }
 
-public class H1{
-    public static void main(String args[]){
+public class Q1 {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the sales:");
-        int commission = sc.nextInt();
-        Commission c1 = new Commission(commission);
-        c1.getCommission();
-
-
-
+        int sales;
+        boolean flag = true;
+        while (flag) {
+            sales = sc.nextInt();
+            if (sales < 0) {
+                System.out.println("Invalid Input");
+            } else {
+                Commission c1 = new Commission(sales);
+                c1.getCommission();
+                flag = false;
+            }
+        }
     }
 }
 
-//here the issue is in question it is not stated that what will be the value of sales from 100 to below 500
+// here the issue is in question it is not stated that what will be the value of
+// sales from 100 to below 500
